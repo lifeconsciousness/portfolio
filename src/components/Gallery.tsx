ee665b4af02a6509f4526e10bf7a96072bf59bfa
Maksym Tovstolis <108622149+lifeconsciousness@users.noreply.github.com>
2025-04-20 23:34:33 +0200

    feat: progress on item rendering

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,16 +2,18 @@ import "../css/gallery.scss";
 import { useRef, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import projects from "../projects/projects.json"
+import projects from "../projects/projects.json";
+import Item from "./Item";
 // import { ScrollTrigger } from 'gsap-trial/ScrollTrigger';
 // import { ScrollSmoother } from 'gsap-trial/ScrollSmoother';
 
+const [renderedItems, setRenderedItems] = useState([]);
 
-const [numItems, setNumItems] = useState(25)
-const [gap, setGap] = useState(70)
-const [numColumns, setnumColumns] = useState(6)
-const [itemWidth, setItemWidth] = useState(130)
-const [itemHeight, setItemHeight] = useState(190)
+const [numItems, setNumItems] = useState(25);
+const [gap, setGap] = useState(70);
+const [numColumns, setnumColumns] = useState(6);
+const [itemWidth, setItemWidth] = useState(130);
+const [itemHeight, setItemHeight] = useState(190);
 
 const isDragging = useRef(false);
 const startX = useRef(0);
@@ -24,7 +26,7 @@ const dragVelocityX = useRef(0);
 const dragVelocityY = useRef(0);
 const lastDragTime = useRef(0);
 const mouseHasMoved = useRef(false);
-const visibleItems = useRef(new Set());
+const visibleItems = useRef(new Set<String>());
 const lastUpdateTime = useRef(0);
 const lastX = useRef(0);
 const lastY = useRef(0);
@@ -40,24 +42,26 @@ const container = useRef(null);
 const canvas = useRef(null);
 const overlay = useRef(null);
 const title = useRef(null) as any;
- 
 
 gsap.registerPlugin(useGSAP); // register any plugins, including the useGSAP hook
-gsap.registerPlugin(SplitText) 
+gsap.registerPlugin(SplitText);
 
-useGSAP(() => {
+useGSAP(
+  () => {
     // gsap code here...
-}, { scope: container }); // <-- scope is for selector text (optional)
+  },
+  { scope: container }
+); // <-- scope is for selector text (optional)
 
 const setupTitle = (t: String) => {
-  if(titleSplit) {
+  if (titleSplit) {
     titleSplit.revert();
   }
   title.innerText = t;
-  titleSplit.current = new SplitText(title, {type: "words"});
-  gsap.set(titleSplit.current, {perspective: 400, y: "100%"})
+  titleSplit.current = new SplitText(title, { type: "words" });
+  gsap.set(titleSplit.current, { perspective: 400, y: "100%" });
   // TODO: make a proper animation
-}
+};
 
 const animateTitleIn = () => {
   gsap.to(titleSplit.current, {
@@ -65,8 +69,8 @@ const animateTitleIn = () => {
     duration: 1,
     stagger: 0.2,
     ease: "power3.out",
-  })
-}
+  });
+};
 
 const animateTitleOut = () => {
   gsap.to(titleSplit.current, {
@@ -74,13 +78,13 @@ const animateTitleOut = () => {
     duration: 1,
     stagger: 0.2,
     ease: "power3.out",
-  })
-}
+  });
+};
 
 const updateVisibleItems = () => {
   const buffer = 2.5;
   const viewWidth = window.innerWidth * (1 + buffer);
-  const viewHeight= window.innerHeight * (1 + buffer);
+  const viewHeight = window.innerHeight * (1 + buffer);
 
   const movingRight = targetX.current > currentX.current;
   const movingDown = targetY.current > currentY.current;
@@ -89,54 +93,88 @@ const updateVisibleItems = () => {
   const directionBufferY = movingDown ? -300 : 300;
 
   const startColumn = Math.floor(
-    (-currentX.current - viewWidth / 2 + (movingRight ? directionBufferX : 0)) / 
-    (itemWidth + gap)
+    (-currentX.current - viewWidth / 2 + (movingRight ? directionBufferX : 0)) /
+      (itemWidth + gap)
   );
 
   const endColumn = Math.floor(
-    (-currentX.current + viewWidth * 1.5 + (!movingRight ? directionBufferX : 0)) / 
-    (itemWidth + gap)
+    (-currentX.current +
+      viewWidth * 1.5 +
+      (!movingRight ? directionBufferX : 0)) /
+      (itemWidth + gap)
   );
 
   const startRow = Math.floor(
-    (-currentY.current - viewHeight / 2 + (movingDown ? directionBufferY : 0)) / 
-    (itemWidth + gap)
+    (-currentY.current - viewHeight / 2 + (movingDown ? directionBufferY : 0)) /
+      (itemWidth + gap)
   );
 
   const endRow = Math.floor(
-    (-currentY.current + viewHeight * 1.5 + (!movingDown ? directionBufferY : 0)) / 
-    (itemWidth + gap)
+    (-currentY.current +
+      viewHeight * 1.5 +
+      (!movingDown ? directionBufferY : 0)) /
+      (itemWidth + gap)
   );
 
   const currentItems = new Set();
+  const projectList = projects.projects;
 
-  for(let row = startRow; row <= endRow; row++){
-    for(let col = startColumn; col <= endColumn; col++){
+  const newItems = [] as any;
+
+  for (let row = startRow; row <= endRow; row++) {
+    for (let col = startColumn; col <= endColumn; col++) {
       const itemId = `${col},${row}`;
       currentItems.add(itemId);
 
-      if(visibleItems.current.has(itemId)) continue;
-      if(activeItemId.current === itemId && isExpanded) continue;
+      if (visibleItems.current.has(itemId)) continue;
+      if (activeItemId.current === itemId && isExpanded) continue;
 
-      
+      const left = `${col * (itemWidth + gap)}px`;
+      const top = `${row * (itemHeight + gap)}px`;
 
+      const index = Math.abs((col + row) * numColumns) % projectList.length;
+      const project = projectList[index];
+      const src = project?.src || "";
+      const alt = project?.name || "";
+
+      newItems.push(
+        <Item
+          key={itemId}
+          id={itemId}
+          left={left}
+          top={top}
+          col={col}
+          row={row}
+          imgSrc={src}
+          imgAlt={alt}
+          action={() => {
+            if (mouseHasMoved.current || isDragging.current) return;
+            // handleItemClick();
+          }}
+        />
+      );
+
+      visibleItems.current.add(itemId);
     }
   }
-   
 
-}
+  visibleItems.current.forEach((itemId) => {
+    if (
+      !currentItems.has(itemId) ||
+      (activeItemId.current === itemId && isExpanded)
+    ) {
+      visibleItems.current.delete(itemId);
+    }
+  });
 
-
-
- 
-
-
+  setRenderedItems(newItems);
+};
 
 function Gallery() {
   return (
     <>
       <main className="container" ref={container}>
-        <div className="canvas" ref={canvas}></div>
+        <div className="canvas" ref={canvas}>{renderedItems}</div>
         <div className="overlay" ref={overlay}></div>
       </main>
 
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,9 +1,29 @@
 // import "/css/main.scss";
 
-function Item() {
+function Item({
+  id,
+  left,
+  top,
+  col,
+  row,
+  imgSrc,
+  imgAlt,
+  action,
+}: {
+  id: String;
+  left: string;
+  top: string;
+  col: Number;
+  row: Number;
+  imgSrc: string;
+  imgAlt: string;
+  action: () => void
+}) {
   return (
     <>
-      <div className="item"></div>
+      <div className="item" data--id={id} style={{left: left, top: top}} onClick={action}>
+        <img src={imgSrc} alt={imgAlt} />
+      </div>
     </>
   );
 }
